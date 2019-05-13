2f6e08d1085800c5932003e39f3572465cde697a
Luis Arevalo <arevaloluis89@yahoo.com>
2019-05-13 01:06:20 -0700

    deployment

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+//production
+
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
+const path = require("path");
+const port = process.env.PORT || 3036;
 
 const app = express();
 app.use(cors());
@@ -88,6 +92,27 @@ app.get("/legacymoves", (req, res) => {
   });
 });
 
-app.listen(3036, () => {
-  console.log("data on port 3036");
+// app.listen(3036, () => {
+//   console.log("data on port 3036");
+// });
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, "client/build")));
+
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
+
+//start server
+app.listen(port, (req, res) => {
+  console.log(`server listening on port: ${port}`);
 });
