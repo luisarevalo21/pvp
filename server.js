a94cd644c6d8e4b1652a2031579a7148af2cc25e
Luis Arevalo <arevaloluis89@yahoo.com>
2019-05-13 14:16:16 -0700

    testing

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get("/chargemoves", (req, res) => {
   });
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join((__dirname, "client/build", "index.html")));
 });
 
