bfc730affacdb169267c13ba775932e9b01fdbc9
Luis Arevalo <arevaloluis89@yahoo.com>
2019-05-16 18:02:45 -0700

    fixed move bug

--- server.js
+++ server.js
@@ -89,7 +89,8 @@ app.get("/pokemon", (req, res) => {
 app.get("/", (req, res) => {
   // res.sendFile(__dirname + "client/build/index.html");
 
-  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+  // res.sendFile(path.join(__dirname + "./client/build/index.html"));
+  res.sendFile(path.resolve(__dirname + "/app/client/build/index.html"));
 });
 // app.get("*", function(req, res) {
 //   const index = path.join(__dirname, "build", "index.html");
